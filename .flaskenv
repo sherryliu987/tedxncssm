026d928c640c984b7224f83eaf2317e113ae0bf5
Sherry <sherryliu987@gmail.com>
2020-12-12 18:06:57 -0500

    initial commit

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+export FLASK_ENV = development
+FLASK_APP=main.py
---
+++ application/routes.py
@@ -0,0 +1,28 @@
+from flask import render_template
+
+from application import app
+
+
+@app.route("/")
+@app.route("/main")
+def home():
+    return render_template("main.html")
+
+
+@app.route("/auditorium")
+def auditorium():
+    return render_template("auditorium.html")
+
+
+@app.route("/exhibits")
+def exhibits():
+    return render_template("exhibits.html")
+
+
+@app.route("/committee")
+def committee():
+    return render_template("committee.html")
+
+@app.route("/photobooth")
+def photobooth():
+    return render_template("photobooth.html")
